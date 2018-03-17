01ebe08f7ed5e2741767b1038b9421b18d6538b4
jochen <jochen.weber80@gmail.com>
2018-03-17 13:22:14 +0100

    add admin auth config variables

--- kong/provider.go
+++ kong/provider.go
@@ -16,6 +16,18 @@ func Provider() terraform.ResourceProvider {
 				DefaultFunc: envDefaultFuncWithDefault("KONG_ADMIN_ADDR", "http://localhost:8001"),
 				Description: "The address of the kong admin url e.g. http://localhost:8001",
 			},
+			"kong_admin_user": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: envDefaultFuncWithDefault("KONG_ADMIN_USER", ""),
+				Description: "An basic auth user for kong admin",
+			},
+			"kong_admin_pass": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: envDefaultFuncWithDefault("KONG_ADMIN_PASS", ""),
+				Description: "An basic auth password for kong admin",
+			},
 		},
 
 		ResourcesMap: map[string]*schema.Resource{
@@ -56,6 +68,8 @@ func envDefaultFuncWithDefault(key string, defaultValue string) schema.SchemaDef
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 	config := &gokong.Config{
 		HostAddress: d.Get("kong_admin_uri").(string),
+		Username: d.Get("kong_admin_user").(string),
+		Password: d.Get("kong_admin_pass").(string),
 	}
 
 	return gokong.NewClient(config), nil
