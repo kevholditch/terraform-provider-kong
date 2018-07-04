a5c9fdf5c05cec1e2a37e167ee74daacd2e1a257
Kamil Szczygiel <kamil@szczygiel.io>
2018-07-04 12:12:37 +0200

    change import

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/kamsz/terraform-provider-kong/kong"
+	"github.com/kevholditch/terraform-provider-kong/kong"
 )
 
 func main() {
